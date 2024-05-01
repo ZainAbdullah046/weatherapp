83dd7306f36c89db3d551630d993e986adfc469f
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-05-01 11:13:20 +0500

    weather application

---
+++ lib/weather.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class weather extends StatefulWidget {
+  const weather({super.key});
+
+  @override
+  State<weather> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<weather> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
